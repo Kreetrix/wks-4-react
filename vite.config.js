073e16a7cb67e4ea1492ca25850ba87ef103c322
kreetz <bronze.reach@gmail.com>
2025-04-13 22:38:59 +0300

    routing fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kreetrix/routing/',
+  base: '/~vladimka/wsk-routing/',
 });
